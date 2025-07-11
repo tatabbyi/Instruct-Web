2842b7dbb8c7316dc959f74361acb73194d8d40a
tatabbyi <ytrachel0@gmail.com>
2025-07-11 12:12:58 +0100

    backend connection

--- backend/instruct/instruct/src/main/java/com/Tatabbyi/instruct/HelloController.java
+++ backend/instruct/instruct/src/main/java/com/Tatabbyi/instruct/HelloController.java
@@ -1,10 +1,13 @@
 package com.Tatabbyi.instruct;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @CrossOrigin(origins ="http://localhost:5500")
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello from Spring Boot!";
