4f12ef3580311465af93c196402ac6857656fce7
tatabbyi <ytrachel0@gmail.com>
2025-07-17 13:30:15 +0100

    key:sad

--- backend/instruct/instruct/src/main/java/com/Tatabbyi/instruct/CopingController.java
+++ backend/instruct/instruct/src/main/java/com/Tatabbyi/instruct/CopingController.java
@@ -28,6 +28,19 @@ public class CopingController {
 
         ));
 
+        strategies.put("sad", Arrays.asList(
+            "Set Goals for adressing sadness: Keep a journal to track your mood and express your sadness",
+            "Identify what it interfers with: Work, school, relationships. Think about exactly how it causes problems in these situations",
+            "It’s crucial to remember that you matter, too. Prioritize your self-care and mental health",
+            "Point out negative thoughts and settle them with positive ones.",
+            "Step away to somewhere quiet and take the time necessary to center yourself until you’re better able to face the stressor. ",
+            "Sometimes we just have to feel emotions and there isnt one strategy to fix it all. You can only try your best.",
+            "Consider any previous progress made in managing sadness or what progress you would like to see",
+            "<a href=\"https://books.google.ie/books?hl=en&lr=&id=uLe2fJRHQGoC&oi=fnd&pg=PA121&dq=dealing+with+sadness&ots=m4sfs7v-55&sig=_tBaIdU8aqYYGyfQMCXJbbG94UA&redir_esc=y#v=onepage&q=dealing%20with%20sadness&f=false\">Re-constructing Emotional Spaces: From Experience to Regulation</a>."
+        ));
+
+
+
         return strategies.getOrDefault(emotion.toLowerCase(), Arrays.asList("Strategies yet to be updates"));
     }
     
