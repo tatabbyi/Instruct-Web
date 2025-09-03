486fbddbfdc9c84a5db025cdb2d170ab181952b3
tatabbyi <ytrachel0@gmail.com>
2025-09-03 12:07:07 +0100

    User Service

---
+++ backend/instruct/instruct/src/main/java/com/Tatabbyi/instruct/service/UserService.java
@@ -0,0 +1,33 @@
+package com.Tatabbyi.instruct.service;
+
+import com.instruct.entity.User;
+import com.instruct.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    public User createUser(String email, String username) {
+        User user = new User(email, username);
+        return userRepository.save(user);
+    }
+
+    public Optional<user> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    
+}
